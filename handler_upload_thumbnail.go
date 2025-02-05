4debc2a6065db9b6f9d1cd701928162edfd17c38
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2025-02-05 08:12:45 -0500

    Create a new file for image data that is uploaded

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"net/http"
     "io"
+    "os"
+    "bytes"
+    "mime"
+    "path/filepath"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
@@ -40,13 +44,38 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
         return;
     }
 
-    media_type := header.Header.Get("Content-Type");
-    
+    media := header.Header.Get("Content-Type");
+    media_type, _, err_mime := mime.ParseMediaType(media);
+    if err_mime != nil || media_type != "image/png" || media_type != "image/jpg"{
+        respondWithError(w, http.StatusInsufficientStorage, "Failed Creating the path", err);
+        return;
+    }
+
+    file_path := filepath.Join("assets", videoIDString + "." + media_type[6:]);
+    fmt.Printf("%s\n", file_path);
+
+    fd, err_create := os.Create(file_path);
+    if err_create != nil{
+        respondWithError(w, http.StatusInsufficientStorage, "Failed Creating the path", err);
+        return;
+    }
+
+
     image_bytes, err_read := io.ReadAll(file);
     if err_read != nil{
         respondWithError(w, http.StatusBadRequest, "Failed reading bytes", err_read);
         return;
     }
+
+    image_bytes_reader := bytes.NewReader(image_bytes);
+    _, err_cpy := io.Copy(fd, image_bytes_reader);
+    if err_cpy != nil{
+        respondWithError(w, http.StatusBadRequest, "Failed reading bytes", err_read);
+        return;
+    }
+
+
+    data_url := fmt.Sprintf("http://localhost:8091/assets/%s.%s", videoIDString, media_type);
 
 
     video, err_query := cfg.db.GetVideo(videoID);
@@ -55,15 +84,8 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
         return;
     }
 
-    tbn := thumbnail{
-        data: image_bytes,
-        mediaType: media_type,
-    };
 
-    videoThumbnails[videoID] = tbn;
-
-    thumbnailURL := ("http://localhost:8090/api/thumbnails/" + videoIDString);
-    video.ThumbnailURL = &thumbnailURL;
+    video.ThumbnailURL = &data_url;
     err_update := cfg.db.UpdateVideo(video);
     if err_update != nil{
         respondWithError(w, http.StatusBadRequest, "Failed reading bytes", err_read);
